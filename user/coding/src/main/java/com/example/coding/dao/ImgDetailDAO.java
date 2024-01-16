317941c533c60479840af43fcd444b1cd3d6d94e
bmh <bmh5543@gmail.com>
2024-01-16 10:27:07 +0900

    1/16-배민호

---
+++ user/coding/src/main/java/com/example/coding/dao/ImgDetailDAO.java
@@ -0,0 +1,14 @@
+package com.example.coding.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.coding.domain.ImgDetailVO;
+
+@Mapper
+public interface ImgDetailDAO {
+
+    public void             insertFile(ImgDetailVO vo);
+    public ImgDetailVO      selectFile(ImgDetailVO vo);
+    public Long             selectNum();
+    
+}
