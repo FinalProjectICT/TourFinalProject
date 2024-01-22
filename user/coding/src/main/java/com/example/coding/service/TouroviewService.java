7608ea6edf3312471b20ad11d8fee312c9c06e33
HSK <gkstnrud012@naver.com>
2024-01-22 12:42:00 +0900

    1/22-한수경-touroview페이지, mypage 페이지

--- user/coding/src/main/java/com/example/coding/service/TouroviewService.java
+++ user/coding/src/main/java/com/example/coding/service/TouroviewService.java
@@ -7,7 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.example.coding.domain.TourVO;
+import com.example.coding.domain.TouroviewReviewVO;
 import com.example.coding.domain.TouroviewVO;
+import com.example.coding.domain.UserVO;
 
 public interface TouroviewService {
     
@@ -18,28 +20,34 @@ public interface TouroviewService {
     // 후기 게시판 작성 후 db 저장
     public void saveTouroview(TouroviewVO touroviewVO);
 
+    
+    // ------------------------------ touroview_list 
+    // (list 페이지) 페이징, 목록
+    public List<TouroviewVO> getTouroviewList(String searchKeyword, int page, int size);
+    
+    // 
+    public int getTotalPages(int size);
+    
+    // ------------------------------ touroview_detail
+    // 게시물 id로 touroview 게시물 가져오기
+    public TouroviewVO getTouroviewById(int touroview_num);
+    
+    // 여행지 번호를 이용하여 해당 여행지 정보를 가져오기
+    public TourVO getTourByTouroviewId(int touroview_num);
+    
+    // 작성자(사용자) 정보 가져오기
+    public UserVO getUserByTouroviewId(int touroview_num);
+    
+    // 후기 게시물 리뷰 정보 가져오기
+    public TouroviewReviewVO getTouroviewReviewByTouroviewId(int touroview_num);
+    
+    
     // ----------------------------- 수정, 삭제
     // 후기 게시판 수정
     public void updateTouroview(TouroviewVO touroviewVO);
     
     // 후기 게시판 삭제
     public void deleteTouroview(int touroviewNum);
-
-    // ------------------------------ touroview_list 
-    // (list 페이지) 페이징, 목록
-    public List<TouroviewVO> getTouroviewList(String searchKeyword, int page, int size);
-
-    // 
-    public int getTotalPages(int size);
-
-    // ------------------------------ touroview_detail
-    // 게시물 id로 touroview 게시물 가져오기
-    public TouroviewVO getTouroviewById(int touroview_num);
-
-    // 여행지 번호를 이용하여 해당 여행지 정보를 가져오기
-    public TourVO getTourByTouroviewId(int touroview_num);
-
-    
     
     
 } 
