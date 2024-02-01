e944e4d26ebfaaa15ca3ed8f0ada9273425f2b20
bmh <bmh5543@gmail.com>
2024-02-01 10:04:51 +0900

    2/1-1-배민호

--- user/coding/src/main/resources/static/assets/js/footer.js
+++ user/coding/src/main/resources/static/assets/js/footer.js
@@ -33,20 +33,19 @@ $(function () {
         url: "/footerTouroview",
         type: "POST",
         success: function (res) {
-            $('.footer-blog > .col-6').each(function (index) {
+            $('.col-4 > div.blog').each(function (index) {
                 var tourInfo = res[index]; 
                 var newElement = $(
-                    `<div class="media col-6">
-                        <div class="img-part rounded5">
-                          <a href="">
-                            <img src="../${tourInfo.tour_img1_path}" class="img-fluid blur-up lazyload" alt="" />
-                          </a>
+                    `<a href="../touroview/touroview_detail?touroview_num=${tourInfo.touroview_num}">
+                        <img
+                            src="../${tourInfo.tour_img1_path}"
+                            class="img-fluid blur-up lazyload"
+                            alt=""
+                        />
+                        <div class="overlay">
+                            <h6>${tourInfo.touroview_title}</h6>
                         </div>
-                        <div class="media-body">
-                          <h5>${tourInfo.touroview_title}</h5>
-                          <p>${tourInfo.touroview_content}</p>
-                        </div>
-                    </div>`
+                    </a>`
                 );
                 $(this).append(newElement);
                 //console.log(newElement);
